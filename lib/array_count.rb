9e642c7a7da9e6f370f1ba5c1f36b79be926296b
placid-tier-4550 <james.ardery@gmail.com>
2020-09-27 18:45:58 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,7 @@
 def count_strings(array)
   array.count do |index|
-    index == String# Return the total number of strings in the provided array using the count enumerable
+    index == String
+  end# Return the total number of strings in the provided array using the count enumerable
 end
 
 def count_empty_strings(array)
